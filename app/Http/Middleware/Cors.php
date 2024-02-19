c61d25e976d34828acc57da8b038e0fad5647f30
prabin-bhusal <prabinbhusal.np@gmail.com>
2024-02-19 10:18:01 +0545

    cors error fixes

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,6 +9,9 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*');
+            ->header('Access-Control-Allow-Origin', 'http://localhost:5173')
+            ->header('Access-Control-Allow-Methods', 'POST')
+            ->header('Access-Control-Allow-Headers', 'Content-Type')
+            ->header('Acces-Control-Allow-Credentials', 'true');
     }
 }
